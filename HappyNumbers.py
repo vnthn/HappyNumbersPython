95d74b1f0fbefbde015ec1a5bf68fa375f1112a5
stnineth <stnineth@stud.h-da.de>
2018-04-25 11:24:24 +0200

    Alles fertig. Gleich mal sehen, ob die Test funktionieren.

--- HappyNumbers.py
+++ HappyNumbers.py
@@ -1,10 +1,18 @@
 import unittest
-
+def sum_of_square_of_digits_of(n):
+   sum = 0
+   while n:
+      sum += (n % 10) * (n % 10)
+      n = int(n/10)
+   return sum
 def is_happy_number(x):
-    if x == 1:
+    if (x <= 0) or (x == 4):
+        return False
+    y = sum_of_square_of_digits_of(x)
+    if y == 1:
         return True
-    
-    
+    else:
+        is_happy_number(y)
 class MyTest(unittest.TestCase):
     def test_happy_number_function(self):
         self.assertTrue(is_happy_number(1))
@@ -13,6 +21,5 @@ class MyTest(unittest.TestCase):
         self.assertFalse(is_happy_number(4))
         self.assertFalse(is_happy_number(0))
         self.assertFalse(is_happy_number(-1))
-
 if __name__ == '__main__':
     unittest.main()
