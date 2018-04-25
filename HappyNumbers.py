27e2e7dc46df80af2f07a23d2853e89c1d7a2044
stnineth <stnineth@stud.h-da.de>
2018-04-25 11:43:02 +0200

    weitere Optimierungen, SummenQuadrat-Funktion wieder entfernt - Tests werden jetzt noch schnneller ausgeführt, yay

--- HappyNumbers.py
+++ HappyNumbers.py
@@ -1,17 +1,14 @@
 import unittest
-def sum_of_square_of_digits_of(n):
-   sum = 0
-   while n:
-      sum += (n % 10) * (n % 10)
-      n = int(n/10)
-   return sum
-def is_happy_number(x):
-    if (x <= 0) or (x == 4):
+def is_happy_number(input):
+    if (input <= 0) or (input in [4, 16, 20, 37, 42, 58, 89, 142]):
         return False
-    y = sum_of_square_of_digits_of(x)
-    if y == 1:
+    sum = 0
+    while input:
+      sum += (input % 10) ** 2
+      input = int(input / 10)
+    if sum == 1:
         return True
-    return is_happy_number(y)
+    return is_happy_number(sum)
 class MyTest(unittest.TestCase):
     def test_happy_number_function(self):
         self.assertTrue(is_happy_number(1))
